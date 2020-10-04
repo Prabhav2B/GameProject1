6d088b0581b6f028655f5735bb6c5e3a11e357d4
Prabhav Bhatnagar <prabhav2b@gmail.com>
2020-10-05 01:27:51 +0530

    added basic animation script

---
+++ Assets/Scripts/PlayerAnimManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerAnimManager : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        anim = this.GetComponentInChildren<Animator>();
+
+        if (anim == null)
+        {
+            Debug.LogError("Missing Animator!");
+        }
+    }
+
+    private void Update()
+    {
+        anim.SetFloat("Horizontal", rb.velocity.x);
+        anim.SetFloat("Vertical", rb.velocity.y);
+
+    }
+}
