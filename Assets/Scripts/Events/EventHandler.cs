524ec2acb59741d146dc9b485494f4fed07d7dd7
Prabhav Bhatnagar <prabhav2b@gmail.com>
2020-10-08 00:27:59 +0530

    rock resetting added

--- Assets/Scripts/Events/EventHandler.cs
+++ Assets/Scripts/Events/EventHandler.cs
@@ -3,13 +3,34 @@ using UnityEngine;
 
 public class EventHandler : MonoBehaviour
 {
-    [SerializeField] List<TriggeredEvent> Events;
+    [SerializeField] List<TriggeredEvent> PassiveEvents;
+    [SerializeField] List<TriggeredEvent> ActiveEvents;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    bool hasActivated = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            hasActivated = true;
+            Invoke("Deactivate", .1f);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.layer == 8)
         {
-            foreach (var ev in Events)
+            if (hasActivated)
+            {
+                hasActivated = false;
+                foreach (var ev in ActiveEvents)
+                {
+                    ev.OnEvent();
+                }
+            }
+
+            foreach (var ev in PassiveEvents)
             {
                 ev.OnEvent();
             }
@@ -22,5 +43,10 @@ public class EventHandler : MonoBehaviour
         Gizmos.DrawWireSphere(this.transform.position + new Vector3(this.GetComponent<CircleCollider2D>().offset.x, 
             this.GetComponent<CircleCollider2D>().offset.y, 0),
             this.GetComponent<CircleCollider2D>().radius);
+    }
+
+    private void Deactivate()
+    {
+        hasActivated = false;
     }
 }
---
+++ Assets/Scripts/ResetRocks.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class ResetRocks : MonoBehaviour
+{
+    private Vector2 myPos;
+    private Rigidbody2D rb;
+
+    [SerializeField] private DeathController dc;
+
+    void Start()
+    {
+        
+        myPos = this.transform.position;
+        rb = this.GetComponent<Rigidbody2D>();
+
+        dc.OnDeath += ResetSelf;
+    }
+
+    void ResetSelf()
+    {
+        rb.velocity = Vector2.zero;
+        rb.angularVelocity = 0f;
+        rb.isKinematic = true;
+        rb.MovePosition(myPos);
+    }
+}
