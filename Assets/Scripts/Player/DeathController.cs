7bc2d4082eebb5f19d7eb7519691138329e13d19
Prabhav Bhatnagar <prabhav2b@gmail.com>
2020-10-12 15:59:29 +0530

    death animation

--- Assets/Scripts/Player/DeathController.cs
+++ Assets/Scripts/Player/DeathController.cs
@@ -1,15 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class DeathController : MonoBehaviour
 {
     [SerializeField] private Transform respawnTransform;
     [SerializeField] int killPlayerLayer = 9;
     [SerializeField] GameObject player;
+    [SerializeField] GameObject deathParticles;
+    [SerializeField] Transform particlesParent;
+
 
     public Transform RespawnTransform { private get { return respawnTransform; } set { respawnTransform = value; } }
     
     public delegate void deathDel();
     public event deathDel OnDeath;
+
+    private void Start()
+    {
+        OnDeath += DeathParticles;
+    }
 
     void FixedUpdate()
     {
@@ -32,10 +41,19 @@ public class DeathController : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == killPlayerLayer)
-        { 
-            this.transform.position = RespawnTransform.position;
+        {
             OnDeath();
+
+            this.transform.position = RespawnTransform.position;
         }
     }
+
+    private void DeathParticles()
+    {
+        GameObject go = Instantiate(deathParticles, this.transform.position, Quaternion.identity);
+        go.transform.parent = particlesParent;
+        Destroy(go, 1f);
+    }
+
 
 }
