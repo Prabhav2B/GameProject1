e0f4e3390474aa41d916df4b2af56e1aad60cb65
Prabhav Bhatnagar <prabhav2b@gmail.com>
2020-10-13 16:53:48 +0530

    audio test

---
+++ Assets/Scripts/SoundScripts/AudioPool.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPool : MonoBehaviour
+{
+    [SerializeField] private int poolSize = 10;
+    [SerializeField] GameObject AudioSourcePrefab;
+
+
+    
+    void Start()
+    {
+        if (this.transform.childCount != 0)
+        {
+            foreach (AudioSource tr in this.transform.GetComponentsInChildren<AudioSource>())
+            {
+                Destroy(tr.gameObject);
+            }
+        }
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            Instantiate(AudioSourcePrefab, this.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SoundScripts/ParticleSound.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(ParticleSystem))]
+[RequireComponent(typeof(AudioSource))]
+
+public class ParticleSound : MonoBehaviour
+{
+
+    AudioSource au;
+    ParticleSystem ps;
+
+    [SerializeField] private AudioClip[] effectClip;
+
+    bool clipPlayed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        au = GetComponent<AudioSource>();
+
+        au.clip = effectClip[Random.Range(0, effectClip.Length)];
+        clipPlayed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ps.subEmitters.GetSubEmitterSystem(0).isPlaying && clipPlayed == false)
+        {
+            au.clip = effectClip[Random.Range(0, effectClip.Length)];
+            au.Play();
+            clipPlayed = true;
+        }
+        else
+        {
+            clipPlayed = false;
+        }
+    }
+}
