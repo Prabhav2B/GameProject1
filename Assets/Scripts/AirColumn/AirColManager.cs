ffd14f254ca68aca79a79c6764348f7a0a373d4d
Prabhav Bhatnagar <prabhav2b@gmail.com>
2020-10-15 01:47:16 +0530

    added steam mechanics and steam particles fix

---
+++ Assets/Scripts/AirColumn/AirColManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AirColumn))]
+public class AirColManager : MonoBehaviour
+{
+
+    AirColumn airColumn;
+    [SerializeField]private float airTime = 5f;
+
+    float timer;
+    bool flip = true;
+
+
+    void Start()
+    {
+        timer = 0;
+        airColumn = this.GetComponent<AirColumn>();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= airTime)
+        {
+            timer = 0;
+            airColumn.Flip();
+        }
+    }
+}
---
+++ Assets/Scripts/AirColumn/AirColumn.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class AirColumn : MonoBehaviour
+{
+    [SerializeField] private float force = 10f;
+    private bool flipped = false;
+
+    private ParticleSystem steamFx;
+
+    private void Start()
+    {
+        steamFx = this.GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(!flipped)
+            collision.gameObject.GetComponent<PlayerController>().ExternalForce( (Vector2)this.transform.up * force);
+        else
+            collision.gameObject.GetComponent<PlayerController>().ResetExternalForce();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+            collision.gameObject.GetComponent<PlayerController>().ResetExternalForce();
+    }
+
+    public void Flip()
+    {
+        flipped = !flipped;
+
+        if (flipped)
+        {
+            steamFx.Stop();
+        }
+        else
+        {
+            steamFx.Play();
+        }
+
+    }
+}
